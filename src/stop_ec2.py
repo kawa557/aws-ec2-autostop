b8610eb099aef0fe56625558bbff0ac261a0a053
EC2 Default User <ec2-user@ip-172-31-47-73.ap-northeast-1.compute.internal>
2024-11-11 05:09:43 +0000

    fix src

--- src/stop_ec2.py
+++ src/stop_ec2.py
@@ -3,14 +3,21 @@ import logging
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+ec2 = boto3.client('ec2')
 
 def handler(event, context):
-    ec2 = boto3.client('ec2')
-
     # Stop EC2 instances
-    instances = ec2.describe_instances(Filters=[{'Name': 'tag:shutdown', 'Values': ['true']}])
-    for reservation in instances['Reservations']:
-        for instance in reservation['Instances']:
-            instance_id = instance['InstanceId']
-            ec2.stop_instances(InstanceIds=[instance_id])
-            logger.info(f'Stopped EC2 instance: {instance_id}')
+    try:
+        instances = ec2.describe_instances(Filters=[
+            {'Name': 'tag:shutdown', 'Values': ['true']},
+            {'Name': 'instance-state-name', 'Values': ['running']}
+        ])
+        for reservation in instances['Reservations']:
+            for instance in reservation['Instances']:
+                instance_id = instance['InstanceId']
+                ec2.stop_instances(InstanceIds=[instance_id])
+                logger.info(f'Stopped EC2 instance: {instance_id}')
+
+    except Exception as e:
+        logger.error('An error occurred')
+        logger.error(e)
